cdf738ed0a9748cf33a9a3439910df329e6ab8f8
Jim Bergstrom <james.bergstrom@fastmail.com>
2022-02-01 22:24:57 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using MoreLinq;
+using Spectre.Console;
+
+var count = Convert.ToInt32(args[0]);
+
+var words = await File.ReadAllLinesAsync("wordlist.txt");
+
+var selectedWords = Enumerable.Range(0,count).Select( i => words[RandomNumberGenerator.GetInt32(words.Length)]).ToList();
+
+var batches = selectedWords.Batch(3);
+
+var tab = new Table();
+tab.Border(TableBorder.None);
+tab.Width(30);
+tab.AddColumns("first","second","third");
+tab.HideHeaders();
+
+foreach(var batch in batches) 
+{
+    tab.AddRow(batch.ToArray());
+}
+AnsiConsole.Write(tab);
